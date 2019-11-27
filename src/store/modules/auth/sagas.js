f34b4b8cb4575e5a3e5d816c84f67b78abcf2513
Flavio Miranda <vinniimiranda@gmail.com>
2019-11-27 14:15:52 -0300

    default for axios Authorization header

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -9,13 +9,16 @@ export function* signIn({ payload }) {
   try {
     const { email, password } = payload;
 
-    const response = yield call(api.post, '/sessions', { email, password });
+    const response = yield call(api.post, 'sessions', { email, password });
 
     const { token, user } = response.data;
 
     if (!user.provider) {
       toast.error('User not allowed');
+      return;
     }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
     yield put(signInSuccess(token, user));
 
@@ -25,5 +28,36 @@ export function* signIn({ payload }) {
     yield put(signFailure());
   }
 }
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastrado, verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
