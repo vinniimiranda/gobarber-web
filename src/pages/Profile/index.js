3ea8929d51cd4d85b64950ccd754cb19d43834db
Flavio Miranda <vinniimiranda@gmail.com>
2019-11-28 08:48:28 -0300

    User profile  update

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit({}) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,27 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
 
-export default all([]);
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload;
+
+    const profile = Object.assign(
+      { name, email },
+      rest.oldPassword ? rest : {},
+    );
+
+    const respose = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso');
+
+    yield put(updateProfileSuccess(respose.data));
+  } catch (error) {
+    toast.error('Erro ao atualizar perfil');
+
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
