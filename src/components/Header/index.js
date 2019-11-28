3c892a95f86eb7c260f0ff7c5f113e75d14d6e93
Flavio Miranda <vinniimiranda@gmail.com>
2019-11-28 10:07:46 -0300

    Logged user on header info

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -6,7 +7,8 @@ import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
-export default function components() {
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
@@ -19,11 +21,14 @@ export default function components() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Vinicius Miranda</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt=""
             />
           </Profile>
